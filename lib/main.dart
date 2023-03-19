528f61519e7968263a29c3caefe19018841afe71
Ismail KARROUD <ismailkarroud@gmail.com>
2023-03-19 11:44:15 +0000

    - optimaze loading data in ManualAlert

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:imense/PrincipalPages/Overview.dart';
-import 'package:imense/Widgets/ManualAlert.dart';
+import 'package:imense/PrincipalPages/ManualAlert.dart';
 
 void main() => runApp(MyApp());
 
